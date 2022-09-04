a40136753eac7fc94e2c993e75126f41d8784236
GreekHelois <111210697+GreekHelois@users.noreply.github.com>
2022-09-03 19:19:11 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+game.StarterGui:SetCore("SendNotification", {Title="Fruity Pebbles#3302"; Text="Hahaha Get Trolled SweetHeart <3 IG@GreekHelois"; Duration=50;})
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Wicked Execution System | Version 2.9 | By Liquid", "All")
